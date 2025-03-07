17af3168e1053b4574887eaa27c1401c617d37e6
Aegox <diegoamundaray2017@gmail.com>
2025-03-06 21:29:57 -0500

    add responsive for Hero component

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -3,22 +3,22 @@ import Button from "./Button";
 
 const Hero: React.FC = () => {
   return (
-    <div className="flex items-center justify-center relative w-full h-[900px] overflow-hidden pt-[150px] px-10 z-30">
+    <div className="flex flex-col-reverse gap-15 lg:flex-row items-center justify-center relative w-full h-full lg:h-[900px] overflow-hidden pt-[150px] px-10 z-30 pb-10">
       <img 
         src="/hero_bg2.png" 
         className="absolute top-0 left-0 w-full h-full object-cover animate-move-image z-10" 
         alt="a hero sprite"
       />
       <div className="light-effect"></div>
-      <section className="flex flex-col gap-10 w-[40%] min-w-[600px]">
-        <h1 className="w-[600px] text-[4.1em] leading-[1.4em] font-bold">Maximiza Ventas con Nuestro Terminal POS</h1>
-        <p className="text-[var(--gray-color)] font-normal w-[90%] min-w-[600px]">Nuestro software POS avanzado está diseñado para transformar tus operaciones comerciales. Con sus características potentes y una interfaz intuitiva, mejora la experiencia del cliente y optimiza tu eficiencia global.</p>
+      <section className="flex flex-col gap-10 w-full lg:w-[40%] lg:min-w-[600px]">
+        <h1 className="w-[100%] lg:w-[600px] text-[48px] lg:text-[4.1em] leading-[1.4em] font-bold">Maximiza Ventas con Nuestro Terminal POS</h1>
+        <p className="text-[var(--gray-color)] font-normal w-[100%] lg:w-[90%] lg:min-w-[600px]">Nuestro software POS avanzado está diseñado para transformar tus operaciones comerciales. Con sus características potentes y una interfaz intuitiva, mejora la experiencia del cliente y optimiza tu eficiencia global.</p>
         <div className="flex gap-5" >
           <Button background="black" hover="primary" text="Producto demo" styles="py-[12px] px-[30px]"/>
           <Button background="priamry" hover="black" text="Prueba gratis" styles="py-[12px] px-[30px]"/>
         </div>
       </section>
-      <img src="hero_img.png" alt="a seller employer" className="z-20 object-cover w-[450px] min-w-[350px]"/>
+      <img src="hero_img.png" alt="a seller employer" className="z-20 object-cover w-[90%] h-[60%] lg:w-[450px] lg:min-w-[350px]"/>
     </div>
   );
 };
